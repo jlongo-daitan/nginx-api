7ce05a532b805c6f4335dbcf2945a97ee06c30f6
João Longo <jlongo@daitangroup.com>
2018-04-09 22:35:20 -0300

    Fixed configs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  console.info('Hello World!');
+  res.send('Hello World!');
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.info('Example app listening on port 3000!'));
